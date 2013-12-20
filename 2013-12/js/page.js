bdf9e087c849824fbe15ec7528d2372d2feaf6c7
Zak Fisher <zak.fisher@gmail.com>
2013-12-19 16:17:28 -0800

    basic responsive styles

--- 2013-12/js/page.js
+++ 2013-12/js/page.js
@@ -3,7 +3,6 @@ Page = new function() {
     var viewCampaignBtn = '.view-campaign';
     var hideCampaignBtn = '.hide-campaign';
     var viewMoreBtn = '.view-more *';
-    var scrollBtn = '[data-scroll]';
     function setBrowser() {
         var N= navigator.appName, ua= navigator.userAgent, tem;
         var M= ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
@@ -21,11 +20,17 @@ Page = new function() {
             name : M[0],
             version : M[1]
         });
+        if (browser.name == 'MSIE' && Number(browser.version) <= 8) {
+            alert('IE 8, abort!');
+        }
         $('html').attr('data-platform', browser.platform).attr('data-browser', browser.name).attr('data-version', browser.version);
     }
     function viewCampaign(e) {
         $(e.currentTarget).siblings('.campaign').show();
         if ($(e.currentTarget).siblings('.campaign').find('iframe').length > 0) {
+            // if mobile
+
+            // else
             $(e.currentTarget).siblings('.campaign').find('iframe').attr('src', 'https://news.westfield.com/pub/sf/ResponseForm?_ri_=X0Gzc2X%3DWQpglLjHJlTQGrzaW8fG17UnmcH4YGndDPG3OuPv5zazc8aGIRVXMtX%3DWQpglLjHJlTQGgDh20vbnR3LzcHPjmoNJDzb5bDFAnzgShDJLb&_ei_=EolaGGF4SNMvxFF7KucKuWPnagcYaHkyBaTWAezHHcm1Hs5nw4zpoIw');
         }
     }
@@ -36,49 +41,20 @@ Page = new function() {
         }
     }
     function toggleViewMore(e) {
-        var btn = $(e.currentTarget);
-        var more = btn.parents('[data-article]').find('div.more');
-        var viewMore = btn.parents('div.view-more');
-        if (more.is('.open')) {
-            more.removeClass('open').slideUp();
-            viewMore.find('span').text('view more');
+        var viewMore = $(e.currentTarget).parents('div.view-more');
+        if (viewMore.find('span').text() == 'view full interview') {
+            $('div.view-more div').addClass('wf-icon-wire-minus').removeClass('wf-icon-wire-add');
+            $('div.view-more span').text('hide full interview');
+            $('.more').slideDown();
         }
         else {
-            more.addClass('open').slideDown();
-            viewMore.find('span').text('view less');
+            $('div.view-more div').addClass('wf-icon-wire-add').removeClass('wf-icon-wire-minus');
+            $('div.view-more span').text('view full interview');
+            $('.more').slideUp();
         }
-    }
-    function scrollPage(e) {
-        var target = $(e.currentTarget);
-        var top = 0;
-        switch (target.attr('data-scroll')) {
-            case 'au':
-                top = $('[data-article="1"]').offset().top;
-                break;
-            case 'uk':
-                top = $('[data-article="2"]').offset().top;
-                break;
-            case 'us':
-                top = $('[data-article="3"]').offset().top;
-                break;
-            case 'fb':
-                top = $('[data-role="fb-pdf"]').offset().top;
-                break;
-        }
-        $(window).tween({
-            scroll:{
-                start: window.pageYOffset,
-                stop: top,
-                time: 0,
-                duration:1,
-                units: 'px',
-                effect: 'easeInOut'
-            }
-        }).play();
     }
     $(document).on('click', viewCampaignBtn, viewCampaign);
     $(document).on('click', hideCampaignBtn, hideCampaign);
     $(document).on('click', viewMoreBtn, toggleViewMore);
-    $(document).on('click', scrollBtn, scrollPage);
     setBrowser();
 };
